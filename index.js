17a4b1eb05dcec83a615ededb306263ebf659294
Ilea Beddow <beddow.ilea@gmail.com>
2022-05-09 10:09:00 -1000

    commit tree 783e002be3702b7778754dad55626688689936eb

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Write your solution in this file!
+const employee= {
+    name: "Ilea",
+    streetAddress : {
+        line1 : "123 Walt Disney ",
+        line2 : "Apt 4",
+    },
+    city: "Honolulu",
+    state: "Hawaii",
+    zipCode: "56789",
+
+}
+function updateEmployeeWithKeyAndValue(obj, key, value) {
+    const newObj = { ...obj };
+    newObj[key] = value
+    return newObj
+
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value) {
+    obj[key] = value
+    return obj
+}
+
+function deleteFromEmployeeByKey(obj, key) {
+    const newObj = { ...obj };
+
+    delete newObj[key];
+    return newObj
+}
+
+function  destructivelyDeleteFromEmployeeByKey(obj, key) {
+    delete obj[key];
+    return obj
+}
